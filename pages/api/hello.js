a28975c1c1d486e29efe1e18225837e8bef6cdcf
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-17 18:19:57 +0530

    updated hello.js file to its original position

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const pool = require('./mypool')
 
 export default (req, res) => {
-  pool.query('SELECT * FROM player ORDER BY id ASC')
-    .then(results => {
-      res.status(200).send(results.rows);
-    })
-    .catch(err => {
-      res.status(500)
-    })
+  res.statusCode = 200
+  res.json({ name: 'John Doe' })
 }
