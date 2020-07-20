1ed829daf054d0d742d46e083e9710bcfc94e582
Onkar Hasabe <61231712+onkar-josh@users.noreply.github.com>
2020-07-20 15:38:35 +0530

    database setup with database connection and migrations (#9)
    * added dependency of db-migrate
    
    * added file for database migration
    
    * added migrations
    
    * updated database.json file for change in env variable name
    
    * removed setup function for migrations
    
    * removed setup function from repository migration
    
    * removed setup function from users_repositories migration
    
    * updated sample env file
    
    * added file for postgres connection
    
    * added new line at the end
    
    * added new line
    
    * added default value to migrations

---
+++ config/postgres-config.js
@@ -0,0 +1,11 @@
+const Pool = require(process.env.DB_DRIVER).Pool
+
+const dbConnection = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
+});
+
+module.exports = dbConnection
---
+++ migrations/20200720072737-users.js
@@ -0,0 +1,38 @@
+"use strict";
+
+exports.up = (db, callback) => {
+  db.createTable(
+    "users",
+    {
+      id: {
+        type: "int",
+        notNull: true,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: "string",
+        length: 50,
+        notNull: true,
+      },
+      email: {
+        type: "string",
+        length: 50,
+        notNull: true,
+      },
+      github_handle: {
+        type: "string",
+        length: 30,
+        notNull: true,
+      },
+    },
+    function (err) {
+      if (err) return callback(err);
+      return callback();
+    }
+  );
+};
+
+exports.down = (db, callback) => {
+  db.dropTable("users", callback);
+};
