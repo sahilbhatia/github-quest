836fd7cf63f535fafe7caa3b88f46a9d48a5769d
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-20 19:19:35 +0530

    postgres pg name added in config

--- config/postgres-config.js
+++ config/postgres-config.js
@@ -1,11 +1,11 @@
-const Pool = require(process.env.DB_DRIVER).Pool
+const Pool = require("pg").Pool
 
-const dbConnection = new Pool({
-  user: process.env.DB_USERNAME,
+const pool = new Pool({
+  user: process.env.DB_USER_NAME,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT),
 });
 
-module.exports = dbConnection
+module.exports = pool
