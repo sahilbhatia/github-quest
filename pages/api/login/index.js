a5220f4d21ef0cf89b87d0128382208cabdee354
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-20 19:20:02 +0530

    added login route

--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -1,12 +1,28 @@
+const jwt = require("jsonwebtoken");
+
 const pool = require("../../../config/postgres-config");
 
 export default async function login(req, res) {
   try {
-    let result = await pool.query("SELECT * FROM users where email = 'onkar.hasabe@joshsoftware.com'")
+    const result = await pool.query("SELECT * FROM users where email = 'onkar.hasabe@joshsoftware.com'")
     if (result.rows[0]) {
-      res.status(200).json(result.rows);
+      const token = jwt.sign(
+        {
+          id: result.rows[0].id,
+          github_handle: result.rows[0].github_handle,
+        },
+        process.env.JWT_SECRET_KEY,
+        {
+          expiresIn: process.env.JWT_EXPIRE_TIME,
+        }
+      );
+      res.status(200).json({
+        data: {
+          token: token,
+        },
+      });
     } else {
-      res.status(400).json({ message: "internal server error" });
+      res.status(401).json({ message: "unauthorized user" });
     }
   } catch {
     res.status(500).json({ message: "internal server error" });
