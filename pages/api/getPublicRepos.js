881175907dc7b8e97d38a4157f7170ea1e1c8708
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-21 17:14:50 +0530

    updated get public repos by changing response

--- pages/api/getPublicRepos.js
+++ pages/api/getPublicRepos.js
@@ -5,12 +5,9 @@ export default async function getPublicRepos(req, res) {
     const userId = await pool.query("select id from users where name = 'onkar'");
     const result = await pool.query("SELECT repositories.* FROM repositories INNER JOIN users_repositories ON users_repositories.repository_id = repositories.id where users_repositories.user_id=" + userId.rows[0].id + "");
     if (result.rows) {
-      res.status(200).json({
-        data: result.rows,
-      });
+      res.status(200).json(result.rows);
     }
-  } catch(err) {
-    console.log(err)
+  } catch {
     res.status(500).json({ message: "internal server error" });
   }
 }
