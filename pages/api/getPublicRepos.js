1a18d0e101c34b330b348111c4928458f342dd7c
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-21 13:52:36 +0530

    added db function for getting public repos from database

---
+++ pages/api/getPublicRepos.js
@@ -0,0 +1,16 @@
+const pool = require("../../../config/postgres-config");
+
+export default async function getPublicRepos(req, res) {
+  try {
+    const userId = await pool.query("select id from repositories where name = 'onkar'");
+
+    const result = await pool.query("SELECT repositories.* FROM repositories INNER JOIN users_repositories ON users_repositories.repository_id = repositories.id where users_repositories.user_id=" + userId.rows[0].id + "");
+    if (result.rows) {
+      res.status(200).json({
+        data: result.rows,
+      });
+    }
+  } catch {
+    res.status(500).json({ message: "internal server error" });
+  }
+}
