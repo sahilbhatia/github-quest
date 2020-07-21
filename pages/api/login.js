f74758f2a9a47bd4a0d8072d36a7975ba21c5751
Avinash Mane <avimanepatil@gmail.com>
2020-07-21 08:47:58 +0530

    added post method for get acces token

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,23 @@
-const data = (req, res) => {
-  res.status(200).send(req.query.code);
+const request = require("superagent");
+
+const data = async (req, res) => {
+  const { code } = req.query;
+  if (!code) {
+    res.status(401).send({
+      message: "Error: no code"
+    });
+  }
+
+  const data = await request
+    .post("https://github.com/login/oauth/access_token")
+    .send({
+      "client_id": "6aa0ca2d884e040075c4",
+      "client_secret": "4e2e9876c9ae01b600deb1d293a9418646b51fb9",
+      "code": code
+    })
+    .set("Accept", "application/json");
+  const access_token = data.body.access_token;
+  res.status(200).send(access_token);
 };
 
 export default data;
