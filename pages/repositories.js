207b32c66be79bb37304f698507b60c7539170e9
Avinash Mane <avimanepatil@gmail.com>
2020-07-22 13:16:24 +0530

    added eact bootstrap table component

--- pages/repositories.js
+++ pages/repositories.js
@@ -1,6 +1,6 @@
 import useSWR from 'swr';
 import styled from "styled-components";
-
+import { Table } from "react-bootstrap"
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Index() {
@@ -8,43 +8,40 @@ export default function Index() {
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
 
-  const Table = styled.table`
-    widTH: 100%;
-    Table-layout: fixed;
-    border:1px solid black;
-    background-color:#e5eaF5
-  `;
-
   const TH = styled.th`
     font-weight: 800;
     font-size: 16px;
     text-transform: uppercase;
-    background-color: rgba(255, 255, 255, 0.3)
   `;
 
   console.log(data);
   return (
     <div>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
       <center>
         <u><h1>Repositories</h1></u>
       </center>
-      <Table>
-        <tr>
-          <TH>Name</TH>
-          <TH>Description</TH>
-          <TH>is_forked</TH>
-          <TH>is_archived</TH>
-          <TH>is_disabled</TH>
-        </tr>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <TH>Name</TH>
+            <TH>Description</TH>
+            <TH>forked</TH>
+            <TH>archived</TH>
+            <TH>disabled</TH>
+          </tr>
+        </thead>
+        <tbody>
         {data.map((item) => (
-          <tr style={{textAlign:"center"}} >
+          <tr >
             <td>{item.name}</td>
             <td>{item.description}</td>
-            <td>{item.is_forked ? <h3 style={{ color: "green" }}> ✔</h3> : <p style={{ color: "brown" }}>✘</p>}</td>
+            <td>{item.is_forked ? <h3 style={{ color: "green",marginTop:"-5px"}}> ✔</h3> : <p style={{ color: "brown" }}>✘</p>}</td>
             <td>{item.is_archived ? <h3 style={{ color: "green" }}> ✔</h3> : <p style={{ color: "brown" }}>✘</p>}</td>
             <td>{item.is_disabled ? <h3 style={{ color: "green" }}> ✔</h3> : <p style={{ color: "brown" }}>✘</p>}</td>
           </tr>
         ))}
+        </tbody>
       </Table>
     </div>
   )
