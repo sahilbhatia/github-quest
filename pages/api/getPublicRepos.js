937b824e2ea52ab2a81b22efd25349dc36361780
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-23 09:29:04 +0530

    wrote query for getting all public repos

--- pages/api/getPublicRepos.js
+++ pages/api/getPublicRepos.js
@@ -2,8 +2,7 @@ const pool = require("../../config/postgres-config");
 
 export default async function getPublicRepos(req, res) {
   try {
-    const userId = await pool.query("select id from users where name = 'onkar'");
-    const result = await pool.query("SELECT repositories.* FROM repositories INNER JOIN users_repositories ON users_repositories.repository_id = repositories.id where users_repositories.user_id=" + userId.rows[0].id + "");
+    const result = await pool.query("SELECT repositories.* FROM repositories INNER JOIN users_repositories ON users_repositories.repository_id = repositories.id");
     if (result.rows) {
       res.status(200).json(result.rows);
     }
