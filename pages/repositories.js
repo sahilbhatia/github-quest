2cc2e315f47087cf423319767df14581ee45c0ba
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-23 18:15:39 +0530

    added pagination with usestate to the repositories page

--- pages/repositories.js
+++ pages/repositories.js
@@ -1,14 +1,18 @@
 import useSWR from 'swr';
 import styled from "styled-components";
+import { useState } from "react";
 import { Table, Button } from "react-bootstrap";
-const fetcher = (url) => fetch(url).then((res) => res.json())
+const fetcher = (url) => fetch(url).then((res) => res.json());
+let limit = 5;
 
 export default function Index() {
-  let limit = 5;
-  let offset = 0;
+
+  let [offset, setOffset] = useState(0);
   let { data, error } = useSWR(`/api/getPublicRepos?limit=${limit}&offset=${offset}`, fetcher);
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
+
+  console.log(data);
   const TH = styled.th`
     font-weight: 800;
     font-size: 16px;
@@ -21,32 +25,24 @@ export default function Index() {
     overflow: hidden;
   `;
   const Pagination = styled.div`
-  position:absolute;
-  bottom:0;
   width:100%;
 `;
   const prev = () => {
-    limit -= 5;
-    offset -= 5;
-    fetcher(`/api/getPublicRepos?limit=${limit}&offset=${offset}`).then(set => {
-      data = set;
-    });
+    offset = offset <= 0 ? 0 : offset - limit;
+    setOffset(offset);
   }
-  const next = () => {
-    limit += 5;
-    offset += 5;
-    fetcher(`/api/getPublicRepos?limit=${limit}&offset=${offset}`).then(set => {
-      data = set;
-    });
+  const next = async () => {
+    offset += limit;
+    setOffset(offset);
   }
   return (
-    <Wrapper >
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
+    <Wrapper className="bg-light grey">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
       <div className="text-center ">
         <u><h1><b>Repositories</b></h1></u>
       </div>
-      <Table hover className="text-center">
-        <thead className="bg-primary" >
+      <Table hover className="text-center" size="sm">
+        <thead className="bg-info text-white" >
           <tr>
             <TH>Sr.No</TH>
             <TH>Name</TH>
@@ -60,20 +56,19 @@ export default function Index() {
           {data.map((item, Index) => (
             <tr >
               <td>{Index + 1}</td>
-              <td className="text-left"><b><a href={item.url}>{item.name}</a></b></td>
+              <td className="text-left"><b><a href={item.url} className="text-primary">{item.name}</a></b></td>
               <td>{item.description == null ? item.description : "No description provided"}</td>
-              <td>{item.is_forked ? <h3 className="text-success" style={{ marginTop: "-5px" }}> ✔</h3> : <p className="text-danger">✘</p>}</td>
-              <td>{item.is_archived ? <h3 className="text-success" style={{ marginTop: "-5px" }}> ✔</h3> : <p className="text-danger">✘</p>}</td>
-              <td>{item.is_disabled ? <h3 className="text-success" style={{ marginTop: "-5px" }}> ✔</h3> : <p className="text-danger">✘</p>}</td>
+              <td>{item.is_forked ? <h3 className="text-success"> ✔</h3> : <p className="text-danger">✘</p>}</td>
+              <td>{item.is_archived ? <h3 className="text-success"> ✔</h3> : <p className="text-danger">✘</p>}</td>
+              <td>{item.is_disabled ? <h3 className="text-success"> ✔</h3> : <p className="text-danger">✘</p>}</td>
             </tr>
           ))}
         </tbody>
       </Table>
-      <Pagination >
-        <Button onClick={prev} >prev</Button>
-        <Button onClick={next}>next</Button>
+      <Pagination className="d-flex justify-content-end">
+        <Button onClick={prev} className="bg-white text-dark">{offset == 0 ? <span className="text-muted">&laquo;</span> : <span>&laquo; {`${offset - 5} - ${offset}`}</span>}</Button>
+        <Button onClick={next} className="mx-5 bg-white text-dark">{`${limit} - ${limit + 5}`} &raquo;</Button>
       </Pagination>
     </Wrapper>
   )
 }
-
