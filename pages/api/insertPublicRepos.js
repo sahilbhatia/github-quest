b778d97277e605aa299d7acd438c425c611b79e2
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-23 09:30:09 +0530

    added cron job for executing insert repo fetching from github

--- pages/api/insertPublicRepos.js
+++ pages/api/insertPublicRepos.js
@@ -7,30 +7,38 @@ let fetchReposAfterTime = process.env.INSERT_REPOS_AFTER_TIME;
 
 export default async function insertPublicRepos(req, res) {
 
-  await cron.schedule("* * * * *", async () => {
-    const data = await request
-      .get("https://api.github.com/users/onkar-josh/repos?since=" + fetchReposAfterTime + "&sort=created&direction=asc")
-      .set(headers);
-    await data.body.map(async (item) => {
+  await cron.schedule(process.env.INSERT_PUBLIC_REPOS_SCHEDULE, async () => {
 
-      const result = await pool.query(`select * from repositories where github_repo_id = '${item.id}'`);
+    const usersList = await pool.query(`select id, github_handle from users`);
+    let iterator = 0;
+    while (iterator < usersList.rows.length) {
 
-      if (result.rowCount === 0 && item.fork === false) {
+      const data = await request
+        .get("https://api.github.com/users/" + usersList.rows[iterator].github_handle + "/repos?since=" + fetchReposAfterTime + "")
+        .set(headers);
+      const mapData = await data.body.map(async (item) => {
+        const result = await pool.query(`select * from repositories where github_repo_id = ${item.id}`);
+        
+        if (result.rowCount === 0 && item.fork === false) {
 
-        const insertRepos = await pool.query(`insert into repositories (github_repo_id, name, url, description, is_forked, is_disabled, is_archived, created_at, updated_at ) values (${item.id},'${item.name}','${item.url}','${item.description}',${item.fork},${item.disabled},${item.archived},'${item.created_at}','${item.updated_at}') returning *`);
-        await pool.query(`insert into users_repositories (user_id,repository_id) values ('1',${insertRepos.rows[0].id})`);
+          const insertRepos = await pool.query(`insert into repositories (github_repo_id, name, url, description, is_forked, is_disabled, is_archived, created_at, updated_at ) values (${item.id},'${item.name}','${item.url}','${item.description}',${item.fork},${item.disabled},${item.archived},'${item.created_at}','${item.updated_at}') returning *`);
+          await pool.query(`insert into users_repositories (user_id,repository_id) values (${usersList.rows[iterator].id},${insertRepos.rows[0].id})`);
+        
+        } else if (result.rowCount === 0 && item.fork == true) {
 
-      } else if (result.rowCount === 0 && item.fork == true) {
+          const parentRepo = await request
+            .get(`https://api.github.com/repos/${usersList.rows[iterator].github_handle}/${item.name}`)
+            .set(headers);
+          const insertRepos = await pool.query(`insert into repositories (github_repo_id, name, url, description, is_forked, is_disabled, is_archived, created_at, updated_at ) values (${item.id},'${item.name}','${item.url}','${item.description}',${item.fork},${item.disabled},${item.archived},'${item.created_at}','${item.updated_at}') returning *`);
+          await pool.query(`insert into users_repositories (user_id,repository_id) values (${usersList.rows[iterator].id},${insertRepos.rows[0].id})`);
+          await pool.query(`insert into forked_repositories (repo_id ,github_parent_repo_id,is_private_parent_repo) values (${insertRepos.rows[0].id},${parentRepo.body.parent.id},${parentRepo.body.parent.private})`);
+        
+        }
 
-        const parentRepo = await request
-          .get(`https://api.github.com/repos/onkar-josh/${result.rows[0].name}`)
-          .set(headers);
-
-        const insertRepos = await pool.query(`insert into repositories (github_repo_id, name, url, description, is_forked, is_disabled, is_archived, created_at, updated_at ) values (${item.id},'${item.name}','${item.url}','${item.description}',${item.fork},${item.disabled},${item.archived},'${item.created_at}','${item.updated_at}') returning *`);
-        await pool.query(`insert into users_repositories (user_id,repository_id) values ('1',${insertRepos.rows[0].id})`);
-        await pool.query(`insert into forked_repositories (repo_id ,github_parent_repo_id,is_private_parent_repo) values (${insertRepos.rows[0].id},${parentRepo.body.parent.id},${parentRepo.body.parent.private})`)
-      }
-    })
+      })
+      await Promise.all(mapData)
+      iterator++;
+    }
     fetchReposAfterTime = moment.utc().format();
   });
   res.status(200).json({
