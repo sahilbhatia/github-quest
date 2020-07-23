72ce9c85ffd30c36aee4a6f2e31f2d05821aa824
Avinash Mane <avimanepatil@gmail.com>
2020-07-23 18:43:33 +0530

    added pagination

--- pages/repositories.js
+++ pages/repositories.js
@@ -1,11 +1,12 @@
 import useSWR from 'swr';
 import styled from "styled-components";
 import { Table, Button } from "react-bootstrap";
+import { useState } from "react";
 const fetcher = (url) => fetch(url).then((res) => res.json())
-
+let limit = 5;
 export default function Index() {
-  let limit = 5;
-  let offset = 0;
+  
+  let [offset, setOffset] = useState(0);
   let { data, error } = useSWR(`/api/getPublicRepos?limit=${limit}&offset=${offset}`, fetcher);
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
@@ -20,33 +21,32 @@ export default function Index() {
     height: 100vh; 
     overflow: hidden;
   `;
-  const Pagination = styled.div`
-  position:absolute;
-  bottom:0;
-  width:100%;
-`;
+  const EmptyList = styled.p`
+    position: fixed;
+    font-size: 16px;
+    border:1px solid black;
+    color: red;
+    margin-left:45%;
+    padding :10px;
+    border-radius: 20px;
+    margin-top:10px;
+  `;
   const prev = () => {
-    limit -= 5;
-    offset -= 5;
-    fetcher(`/api/getPublicRepos?limit=${limit}&offset=${offset}`).then(set => {
-      data = set;
-    });
+    offset = offset <= 0 ? 0 : offset - limit;
+    setOffset(offset);
   }
-  const next = () => {
-    limit += 5;
-    offset += 5;
-    fetcher(`/api/getPublicRepos?limit=${limit}&offset=${offset}`).then(set => {
-      data = set;
-    });
+  const next =async () => {
+    offset += limit;
+    setOffset(offset);
   }
   return (
-    <Wrapper >
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
+    <Wrapper className="bg-light grey">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
       <div className="text-center ">
         <u><h1><b>Repositories</b></h1></u>
       </div>
-      <Table hover className="text-center">
-        <thead className="bg-primary" >
+      <Table hover className="text-center" size="sm">
+        <thead className="bg-info text-white" >
           <tr>
             <TH>Sr.No</TH>
             <TH>Name</TH>
@@ -56,23 +56,23 @@ export default function Index() {
             <TH>Disabled</TH>
           </tr>
         </thead>
-        <tbody>
+        {data.length==0?<EmptyList>List Is Empty...</EmptyList>:<tbody>
           {data.map((item, Index) => (
-            <tr >
-              <td>{Index + 1}</td>
-              <td className="text-left"><b><a href={item.url}>{item.name}</a></b></td>
+            <tr key={Index}>
+              <td>{offset + Index +1}</td>
+              <td className="text-left"><b><a href={item.url} className="text-primary">{item.name}</a></b></td>
               <td>{item.description == null ? item.description : "No description provided"}</td>
-              <td>{item.is_forked ? <h3 className="text-success" style={{ marginTop: "-5px" }}> ✔</h3> : <p className="text-danger">✘</p>}</td>
-              <td>{item.is_archived ? <h3 className="text-success" style={{ marginTop: "-5px" }}> ✔</h3> : <p className="text-danger">✘</p>}</td>
-              <td>{item.is_disabled ? <h3 className="text-success" style={{ marginTop: "-5px" }}> ✔</h3> : <p className="text-danger">✘</p>}</td>
+              <td>{item.is_forked ? <h3 className="text-success"> ✔</h3> : <p className="text-danger">✘</p>}</td>
+              <td>{item.is_archived ? <h3 className="text-success"> ✔</h3> : <p className="text-danger">✘</p>}</td>
+              <td>{item.is_disabled ? <h3 className="text-success"> ✔</h3> : <p className="text-danger">✘</p>}</td>
             </tr>
           ))}
-        </tbody>
+        </tbody>}
       </Table>
-      <Pagination >
-        <Button onClick={prev} >prev</Button>
-        <Button onClick={next}>next</Button>
-      </Pagination>
+      <div className="d-flex justify-content-end">
+          <Button onClick={prev} className="bg-white text-dark">{offset==0?<span className="text-muted">&laquo;</span>:<span>&laquo; {`${offset-limit+1} - ${offset}`}</span>}</Button>
+          <Button onClick={next} className="mx-5 bg-white text-dark">{`${offset+limit +1} - ${limit+offset+limit}`} &raquo;</Button>
+      </div>
     </Wrapper>
   )
 }
