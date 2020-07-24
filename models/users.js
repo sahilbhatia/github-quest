9bb95ff0b95213b9ab38f647516cfe95059e2945
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-24 16:33:21 +0530

    added users model

---
+++ models/users.js
@@ -0,0 +1,44 @@
+module.exports = (sequelize, Sequelize) => {
+  const Users = sequelize.define(
+    "users",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+      github_handle: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      created_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    },
+    {
+      timestamp: false,
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
+  Users.associate = (models) => {
+    Users.hasMany(models.users_repositories, {
+      foreignKey: "user_id",
+    });
+  };
+  return Users;
+};
