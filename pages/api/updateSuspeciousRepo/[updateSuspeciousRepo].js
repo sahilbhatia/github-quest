6244f164afeaacd5bfd7dba8d1215063a1dea85f
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-28 14:17:09 +0530

    added file for update suspicious repo

---
+++ pages/api/updateSuspeciousRepo/[updateSuspeciousRepo].js
@@ -0,0 +1,25 @@
+const dbConn = require("../../../models/sequelize");
+dbConn.sequelize;
+const db = require("../../../models/sequelize");
+const Repositories = db.repositories;
+
+const updateSuspiciousRepos = async (req, res) => {
+  const repoId = req.query.id;
+  try {
+    await Repositories.update({ is_suspicious: true }, {
+      returning: true,
+      where: { id: repoId },
+    });
+
+    res.status(200).json({
+      message: "repo updated successfully"
+    })
+
+  } catch {
+    res.status(500).json({
+      message: "internal server error"
+    })
+  }
+}
+
+export default updateSuspiciousRepos;
