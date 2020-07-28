11b80c5b5237e15b414ed501636bef855af93801
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-07-28 09:39:21 +0530

    updated insert public repos after updation

--- pages/api/insertPublicRepos.js
+++ pages/api/insertPublicRepos.js
@@ -10,7 +10,7 @@ const db = require("../../models/sequelize");
 const Users = db.users;
 const Repositories = db.repositories;
 const Users_repositories = db.users_repositories;
-const Parent_repositories = db.parent_repositories;
+//const Fetch_repos_after_time_intervals = db.fetched_repos_time_intarvals;
 
 export default async function insertPublicRepos(req, res) {
 
@@ -26,6 +26,7 @@ export default async function insertPublicRepos(req, res) {
       const data = await request
         .get("https://api.github.com/users/" + usersList[iterator].dataValues.github_handle + "/repos?since=" + fetchReposAfterTime + "")
         .set(headers);
+
       const mapData = await data.body.map(async (item) => {
 
         const result = await Repositories.findAll({
@@ -39,12 +40,13 @@ export default async function insertPublicRepos(req, res) {
             name: item.name,
             url: item.url,
             description: item.description,
-            is_forked: item.fork,
             is_disabled: item.disabled,
             is_archived: item.archived,
+            is_private: item.private,
             created_at: item.created_at,
             updated_at: item.updated_at,
           })
+
           await Users_repositories.create({
             user_id: usersList[iterator].dataValues.id,
             repository_id: insertRepos.dataValues.id,
@@ -56,9 +58,10 @@ export default async function insertPublicRepos(req, res) {
             .get(`https://api.github.com/repos/${usersList[iterator].dataValues.github_handle}/${item.name}`)
             .set(headers);
 
-          insertParentRepositories = await Parent_repositories.findOne({
+          insertParentRepositories = await Repositories.findOne({
             where: { github_repo_id: parentRepo.body.parent.id }
           })
+
           if (insertParentRepositories) {
 
             const insertRepos = await Repositories.create({
@@ -66,41 +69,68 @@ export default async function insertPublicRepos(req, res) {
               name: item.name,
               url: item.url,
               description: item.description,
-              is_forked: item.fork,
               is_disabled: item.disabled,
               is_archived: item.archived,
+              is_private: item.private,
+              is_forked: true,
               created_at: item.created_at,
               updated_at: item.updated_at,
               parent_repo_id: insertParentRepositories.dataValues.id,
+              is_suspicious: insertParentRepositories.dataValues.is_private,
             })
+
             await Users_repositories.create({
               user_id: usersList[iterator].dataValues.id,
               repository_id: insertRepos.dataValues.id,
             })
 
+            await Repositories.update({ is_forked: true }, {
+              returning: true,
+              where: { id: insertParentRepositories.dataValues.id },
+            });
+
           } else {
-            insertParentRepositories = await Parent_repositories.create({
+
+            insertParentRepositories = await Repositories.create({
               github_repo_id: parentRepo.body.parent.id,
+              name: parentRepo.body.name,
               url: parentRepo.body.parent.url,
+              description: parentRepo.body.parent.description,
               is_private: parentRepo.body.parent.private,
+              is_archived: parentRepo.body.archived,
+              is_disabled: parentRepo.body.disabled,
+              is_forked: true,
+              created_at: parentRepo.body.parent.created_at,
+              updated_at: parentRepo.body.parent.updated_at,
             })
+
+            let userObject = await Users.findOne({
+              where: { github_handle: parentRepo.body.parent.owner.login }
+            })
+
+            // await Users_repositories.create({
+            //   user_id: userObject.dataValues.id,
+            //   repository_id: insertParentRepositories.dataValues.id,
+            // })
 
             const insertRepos = await Repositories.create({
               github_repo_id: item.id,
               name: item.name,
               url: item.url,
               description: item.description,
-              is_forked: item.fork,
               is_disabled: item.disabled,
               is_archived: item.archived,
+              is_private: item.private,
               created_at: item.created_at,
               updated_at: item.updated_at,
               parent_repo_id: insertParentRepositories.dataValues.id,
+              is_suspicious: insertParentRepositories.dataValues.is_private,
             })
-            await Users_repositories.create({
-              user_id: usersList[iterator].dataValues.id,
-              repository_id: insertRepos.dataValues.id,
-            })
+
+            // await Users_repositories.create({
+            //   user_id: usersList[iterator].dataValues.id,
+            //   repository_id: insertRepos.dataValues.id,
+            // })
           }
         }
       })
@@ -113,6 +143,6 @@ export default async function insertPublicRepos(req, res) {
   });
 
   res.status(200).json({
-    message: "data inserted successfully"
+    message: "cron Job Activated successfully"
   })
 }
