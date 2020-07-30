aa175addd28c53da49e66034647f016108d1481c
Avinash Mane <avimanepatil@gmail.com>
2020-07-30 12:05:02 +0530

    added node_env in env.sample file

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.HOST,
-    dialect: "postgres",
+    dialect: process.env.DB_DIALECT,
     logging: true,
   },
   test: {
