1371acd35b130cc01649a4652af55fc1171cc72d
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-08-05 09:42:02 +0530

    added const variable users to insert data of users

--- pages/api/insertProjects.js
+++ pages/api/insertProjects.js
@@ -8,70 +8,79 @@ const Projects = db.projects;
 const Users_projects = db.users_projects;
 
 export default async function insertProjects(req, res) {
-  const intranetProjects = await request
-    .get("https://stage-intranet.joshsoftware.com/api/v1/projects")
-    .set({
-      "Content-Type": "application/json",
-      "Accept": "application/json"
+  try {
+    const intranetProjects = await request
+      .get("https://stage-intranet.joshsoftware.com/api/v1/projects")
+      .set({
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      });
+
+    const listOfProjects = await JSON.parse(intranetProjects.text);
+
+    const insertUsersList = await listOfProjects.projects.map(async (item) => {
+
+      if (item.repositories[0]) {
+        const createdProject = await Projects.create({
+          name: item.name ? item.name : "unknown",
+          repository_url: item.repositories[0].url != 0 ? item.repositories[0].url : null,
+          host: item.repositories[0].host ? item.repositories[0].host : null,
+          org_project_id: item.id
+        })
+
+        if (item.active_users[0]) {
+
+          const insertActiveUsers = await item.active_users.map(async (item) => {
+            try {
+              const User = await Users.findOne({
+                where: {
+                  org_user_id: item.id,
+                }
+              })
+              await Users_projects.create({
+                user_id: User.id,
+                project_id: createdProject.id,
+              })
+            } catch  {
+              return;
+            }
+
+          });
+          await Promise.all(insertActiveUsers)
+        }
+      } else {
+        const createdProject = await Projects.create({
+          name: item.name ? item.name : "unknown",
+          repository_url: null,
+          host: null,
+          org_project_id: item.id
+        })
+
+        if (item.active_users[0]) {
+
+          const insertActiveUsers = await item.active_users.map(async (item) => {
+            try {
+              User = await Users.findOne({
+                where: {
+                  org_user_id: item.id,
+                }
+              })
+
+              await Users_projects.create({
+                user_id: User.id,
+                project_id: createdProject.id,
+              })
+            } catch {
+              return
+            }
+          });
+          await Promise.all(insertActiveUsers)
+
+        }
+      }
     });
-
-  const listOfProjects = await JSON.parse(intranetProjects.text);
-
-  const insertUsersList = await listOfProjects.projects.map(async (item) => {
-   
-    if (item.repositories[0]) {
-      const createdProject = await Projects.create({
-        name: item.name ? item.name : "unknown",
-        repository_url: item.repositories[0].url != 0 ? item.repositories[0].url : null,
-        host: item.repositories[0].host ? item.repositories[0].host : null,
-        org_project_id: item.id
-      })
-
-      if (item.active_users[0]) {
-
-        const insertActiveUsers = await item.active_users.map(async (item) => {
-
-          User = await Users.findOne({
-            where: {
-              org_user_id: item.id,
-            }
-          })
-
-          await Users_projects.create({
-            user_id: User.id,
-            project_id: createdProject.id,
-          })
-
-        });
-        await Promise.all(insertActiveUsers)
-      }
-    } else {
-      const createdProject = await Projects.create({
-        name: item.name ? item.name : "unknown",
-        repository_url: null,
-        host: null,
-        org_project_id: item.id
-      })
-
-      if (item.active_users[0]) {
-
-        const insertActiveUsers = await item.active_users.map(async (item) => {
-
-          User = await Users.findOne({
-            where: {
-              org_user_id: item.id,
-            }
-          })
-
-          await Users_projects.create({
-            user_id: User.id,
-            project_id: createdProject.id,
-          })
-
-        });
-        await Promise.all(insertActiveUsers)
-      }
-    }
-  });
-  await Promise.all(insertUsersList)
+    await Promise.all(insertUsersList)
+  } catch {
+    return;
+  }
 }
