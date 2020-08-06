f73ad6bf9e56bae0c89cb9f902fc73f73736fece
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-08-06 12:20:33 +0530

    updated get projects by adding filters

--- pages/api/[getProjects].js
+++ pages/api/[getProjects].js
@@ -21,7 +21,16 @@ Projects.hasMany(Repositories, { foreignKey: { name: 'project_id', allowNull: tr
 const getProjects = async (req, res) => {
   console.log(req.query)
   try {
-    let data = await Projects.findAll({
+    let {
+      projectName,
+      startDate,
+      endDate,
+    } = req.query;
+    console.log(projectName)
+    console.log(startDate)
+    let where = {};
+    let findAllClause = {};
+    findAllClause = {
       include: [
         {
           model: Users_projects,
@@ -35,15 +44,57 @@ const getProjects = async (req, res) => {
       ],
       limit: req.query.limit,
       offset: req.query.offset,
-    });
-    if (data.length == 0) {
-      res.status(404).json({
-        message: "list not found for given id"
-      });
-    };
-    res.status(200).json(data);
-  } catch (err){
-    console.log(err)
+    }
+
+    const getWhereClause = () => {
+      if (projectName || startDate || endDate) {
+
+        if (projectName != undefined) {
+          where.name = projectName
+        }
+
+        if (startDate != undefined && endDate != undefined) {
+          where.created_at = {
+            [Sequelize.Op.between]: [new Date(startDate), new Date(endDate)]
+          }
+        } else if (endDate != undefined) {
+          where.created_at = {
+            [Sequelize.Op.lt]: new Date(endDate),
+          }
+        } else if (startDate != undefined) {
+          const date = new Date();
+          where.created_at = {
+            [Sequelize.Op.between]: [new Date(startDate), date]
+          }
+        }
+        return where;
+      } else {
+        return null;
+      }
+    }
+
+    const whereClauseData = getWhereClause();
+
+    if (whereClauseData) {
+      findAllClause.where = whereClauseData;
+      let data = await Projects.findAll(findAllClause);
+      if (data.length == 0) {
+        res.status(404).json({
+          message: "list not found for given id"
+        });
+      };
+      res.status(200).json(data);
+    } else {
+      let data = await Projects.findAll(findAllClause);
+      if (data.length == 0) {
+        res.status(404).json({
+          message: "list not found for given id"
+        });
+      };
+
+      res.status(200).json(data);
+    }
+  } catch {
     res.status(500).json({
       message: "internal server error"
     })
