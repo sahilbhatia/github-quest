cc7b41cbaa1cce0104d23bf532e76a2597ba75b7
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-08-07 09:41:43 +0530

    removed bug of include statement

--- pages/api/getUsers.js
+++ pages/api/getUsers.js
@@ -7,8 +7,6 @@ const db = require("../../models/sequelize");
 const Projects = db.projects;
 const Users_projects = db.users_projects;
 const Users = db.users;
-const Repositories = db.repositories;
-
 
 Users_projects.belongsTo(Projects, { foreignKey: { name: 'project_id', allowNull: true } });
 Projects.hasMany(Users_projects, { foreignKey: { name: 'project_id', allowNull: true } });
@@ -23,24 +21,22 @@ const getUsers = async (req, res) => {
       username,
       github_handle,
     } = req.query;
-    let data = await Users.findAll({
-      include :[{
-        model:users_projects,
-        attributes :["id"],
-        where: {project_id: projectId},
-        include :{
-          model: Users,
-          attributes :["id"],
-          include: {
-            model: users_projects,
-          }
-        }
-      },
-    ],
-      
-    });
-    res.status(200).json(data);
+
     let where = {};
+
+    let includeUsersProjects = {
+      model: users_projects,
+      attributes: ["id"],
+      where: { project_id: projectId },
+      include: {
+        model: Users,
+        attributes: ["id"],
+        include: {
+          model: users_projects,
+        }
+      }
+    };
+
     const getWhereClauseProject = () => {
 
       if (projectId != undefined) {
@@ -76,12 +72,13 @@ const getUsers = async (req, res) => {
     if (getwhereClauseObject) {
       findAllData.where = getwhereClauseObject
       let data = await Users.findAll(findAllData);
+
       res.status(200).json(data);
     } else {
       let data = await Users.findAll(findAllData);
       res.status(200).json(data);
     }
-  } catch{
+  } catch {
     res.status(500).json({
       message: "internal server error"
     })
