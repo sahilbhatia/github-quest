570e1cf158edb996e91358ee9695903c403970ac
Avinash Mane <avimanepatil@gmail.com>
2020-08-14 18:12:37 +0530

    added webhook for add user in project and remove from project

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,7 +1,11 @@
+import { projects } from "../../models/sequelize";
+
 const dbConn = require("../../models/sequelize");
 dbConn.sequelize;
 const db = require("../../models/sequelize");
 const Users = db.users;
+const UsersProjects = db.users_projects;
+const Projects = db.projects;
 export default async function insertUsers(req, res) {
 
   try {
@@ -40,7 +44,68 @@ export default async function insertUsers(req, res) {
             })
           })
         }
-        break;
+
+      case "User Added":
+        const project = await Projects.findOne({
+          where: { org_project_id: data.project_id }
+        })
+        if (!project) {
+          res.status(404).json({
+            message: "project not found"
+          })
+        } else {
+          const user = await Users.findOne({
+            where: { org_user_id: data.user_id }
+          })
+          if (!user) {
+            res.status(404).json({
+              message: "user not found"
+            })
+          } else {
+            const insertObj = {
+              project_id: project.id,
+              user_id: user.id
+            }
+            UsersProjects.create(insertObj)
+              .then((res) => {
+                res.status(200).json({
+                  message: "added user in project successfully"
+                })
+              })
+          }
+        }
+
+        case "User Removed":
+        const project = await Projects.findOne({
+          where: { org_project_id: data.project_id }
+        })
+        if (!project) {
+          res.status(404).json({
+            message: "project not found"
+          })
+        } else {
+          const user = await Users.findOne({
+            where: { org_user_id: data.user_id }
+          })
+          if (!user) {
+            res.status(404).json({
+              message: "user not found"
+            })
+          } else {
+            UsersProjects.destroy({
+              where:{
+                project_id: project.id,
+                user_id: user.id 
+              }
+            })
+              .then((res) => {
+                res.status(200).json({
+                  message: "removed user in project successfully"
+                })
+              })
+          }
+        }
+
       default:
         break;
 
