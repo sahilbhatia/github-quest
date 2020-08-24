800b9318c1266b9440f93af3ab8a7b7ad425760a
Avinash Mane <avimanepatil@gmail.com>
2020-08-24 19:46:56 +0530

    added test case get project of user, get users of project, get repositories of project api

---
+++ test/api/getProjectsOfUser.test.js
@@ -0,0 +1,57 @@
+const dbConn = require("../../models/sequelize");
+dbConn.sequelize;
+const faker = require("faker");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const should = require('should');
+chai.use(chaiHttp);
+const app = "http://localhost:3000";
+const db = require("../../models/sequelize");
+const data = require("../data");
+let user = data.user;
+describe("test cases for find user api", function () {
+  let userId;
+
+  before((done) => {
+    db.users.create(user).then((res) => {
+      userId = res.id;
+      done();
+    });
+  });
+
+  after(async () => {
+    await db.users.destroy({ where: { id: userId } });
+  });
+
+  it("find project of user should give status 200", function (done) {
+    chai
+      .request(app)
+      .get(`/api/getProjectsOfUser?userId=${userId}`)
+      .end(function (err, res) {
+        should(res.status).eql(200);
+        done();
+      });
+  });
+
+
+
+  it("find project of invalid user id should give status 400", function (done) {
+    chai
+      .request(app)
+      .get(`/api/getProjectsOfUser?userId=azby12`)
+      .end(function (err, res) {
+        should(res.status).eql(400);
+        done();
+      });
+  });
+
+  it("find project of invalid user id should give status 404", function (done) {
+    chai
+      .request(app)
+      .get(`/api/getProjectsOfUser?userId=12345`)
+      .end(function (err, res) {
+        should(res.status).eql(404);
+        done();
+      });
+  });
+});
