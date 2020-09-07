7e4266a0eccca415ec7d6f276304f62df448a049
Avinash Mane <avimanepatil@gmail.com>
2020-09-07 16:47:31 +0530

    update insertUsers api

--- src/pages/api/insertUsers.js
+++ src/pages/api/insertUsers.js
@@ -21,40 +21,77 @@ export default async function insertUsers(req, res) {
           email: item.email,
         },
       });
-      if (!find_user) {
-        const get_github_handle = () => {
-          if (item.public_profile) {
-            if (!item.public_profile.github_handle) {
-              return null;
-            } else if (item.public_profile.github_handle.includes("/")) {
-              if (item.public_profile.github_handle.includes("?")) {
-                let url = item.public_profile.github_handle;
-                let newUrl = url.split("/").pop();
-                return newUrl.split("?")[0];
-              } else {
-                let url = item.public_profile.github_handle;
-                return url.split("/").pop();
-              }
-            } else {
-              return item.public_profile.github_handle;
-            }
+      const get_github_handle = () => {
+        if (!item.public_profile.github_handle) {
+          return null;
+        } else if (item.public_profile.github_handle.includes("/")) {
+          if (item.public_profile.github_handle.includes("?")) {
+            let url = item.public_profile.github_handle;
+            let newUrl = url.split("/").pop();
+            return newUrl.split("?")[0];
           } else {
-            return null;
+            let url = item.public_profile.github_handle;
+            return url.split("/").pop();
           }
-        };
+        } else {
+          return item.public_profile.github_handle;
+        }
+      };
+      const get_gitlab_handle = () => {
+        if (!item.public_profile.gitlab_handle) {
+          return null;
+        } else if (item.public_profile.gitlab_handle.includes("/")) {
+          if (item.public_profile.gitlab_handle.includes("?")) {
+            let url = item.public_profile.gitlab_handle;
+            let newUrl = url.split("/").pop();
+            return newUrl.split("?")[0];
+          } else {
+            let url = item.public_profile.gitlab_handle;
+            return url.split("/").pop();
+          }
+        } else {
+          return item.public_profile.gitlab_handle;
+        }
+      };
+      const get_bitbucket_handle = () => {
+        if (!item.public_profile.bitbucket_handle) {
+          return null;
+        } else if (item.public_profile.bitbucket_handle.includes("/")) {
+          if (item.public_profile.bitbucket_handle.includes("?")) {
+            let url = item.public_profile.bitbucket_handle;
+            let newUrl = url.split("/").pop();
+            return newUrl.split("?")[0];
+          } else {
+            let url = item.public_profile.bitbucket_handle;
+            return url.split("/").pop();
+          }
+        } else {
+          return item.public_profile.bitbucket_handle;
+        }
+      };
+      if (!find_user) {
         try {
-          const github_handle = await get_github_handle();
+          const github_handle = item.public_profile
+            ? await get_github_handle()
+            : null;
+          const gitlab_handle = item.public_profile
+            ? await get_gitlab_handle()
+            : null;
+          const bitbucket_handle = item.public_profile
+            ? await get_bitbucket_handle()
+            : null;
           const role = await Roles.findOne({
             where: {
               role: item.role,
             },
           });
-
           await Users.create({
             name: item.name ? item.name : "unknown",
             role_id: role.dataValues.id,
             email: item.email,
             github_handle: github_handle,
+            gitlab_handle: gitlab_handle,
+            bitbucket_handle: bitbucket_handle,
             org_user_id: item.id,
           });
         } catch {
@@ -62,32 +99,20 @@ export default async function insertUsers(req, res) {
         }
       } else if (find_user && item.public_profile) {
         try {
-          const get_github_handle = () => {
-            if (item.public_profile) {
-              if (!item.public_profile.github_handle) {
-                return null;
-              } else if (item.public_profile.github_handle.includes("/")) {
-                if (item.public_profile.github_handle.includes("?")) {
-                  let url = item.public_profile.github_handle;
-                  let newUrl = url.split("/").pop();
-                  return newUrl.split("?")[0];
-                } else {
-                  let url = item.public_profile.github_handle;
-                  return url.split("/").pop();
-                }
-              } else {
-                return item.public_profile.github_handle;
-              }
-            } else {
-              return null;
-            }
-          };
           const github_handle = await get_github_handle();
-          if (find_user.dataValues.github_handle != github_handle) {
+          const gitlab_handle = await get_gitlab_handle();
+          const bitbucket_handle = await get_bitbucket_handle();
+          if (
+            find_user.dataValues.github_handle != github_handle ||
+            find_user.dataValues.gitlab_handle != gitlab_handle ||
+            find_user.dataValues.bitbucket_handle != bitbucket_handle
+          ) {
             await Users.update(
               {
                 name: item.name ? item.name : "unknown",
-                github_handle: item.public_profile ? github_handle : null,
+                github_handle: github_handle,
+                gitlab_handle: gitlab_handle,
+                bitbucket_handle: bitbucket_handle,
               },
               {
                 returning: true,
