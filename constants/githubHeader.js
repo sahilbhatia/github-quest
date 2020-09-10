6add2b89487237bc24832fdcca48113e498cccee
Avinash Mane <avimanepatil@gmail.com>
2020-09-10 19:55:59 +0530

    update functionality to insert only public repos

--- constants/githubHeader.js
+++ constants/githubHeader.js
@@ -1,6 +1,6 @@
 export const headers = {
   "User-Agent":
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
-  Authorization: "token " + process.env.ACCESS_TOKEN + "",
+  Authorization: "token " + process.env.GITHUB_ACCESS_TOKEN + "",
   Accept: "application/vnd.github.v3+json",
 };
--- constants/gitlabHeader.js
+++
@@ -1,3 +0,0 @@
-export const headers = {
-  "PRIVATE-TOKEN": process.env.GITLAB_ACCESS_TOKEN,
-};
