fc4e465f34eed4ba2046c3a4e5ead03e0c82bc2f
Avinash Mane <avimanepatil@gmail.com>
2020-09-18 11:42:05 +0530

    modify find user api

--- src/pages/api/findUser.js
+++ src/pages/api/findUser.js
@@ -4,80 +4,95 @@ const db = require("../../../models/sequelize");
 const Users = db.users;
 const yup = require("yup");
 const Sequelize = require("sequelize");
+
+//function for find user by username
+const findUserByUserName = async (userName) => {
+  let userList = await Users.findAll({
+    where: {
+      name: {
+        [Sequelize.Op.iLike]: "%" + userName + "%",
+      },
+    },
+  });
+  return userList;
+};
+
+//function for find user by git handle
+const findUserByGitHandle = async (gitHandle) => {
+  let userList = await Users.findAll({
+    where: {
+      [Sequelize.Op.or]: [
+        {
+          gitlab_handle: {
+            [Sequelize.Op.iLike]: "%" + gitHandle + "%",
+          },
+        },
+        {
+          github_handle: {
+            [Sequelize.Op.iLike]: "%" + gitHandle + "%",
+          },
+        },
+        {
+          bitbucket_handle: {
+            [Sequelize.Op.iLike]: "%" + gitHandle + "%",
+          },
+        },
+      ],
+    },
+  });
+  return userList;
+};
+
+//function for find user by user id
+const findUserByUserId = async (res, userId) => {
+  await yup
+    .object()
+    .shape({
+      userId: yup.number().required({ userId: "required" }),
+    })
+    .validate({
+      userId: userId,
+    })
+    .then(async () => {
+      let user = await Users.findOne({
+        where: {
+          id: userId,
+        },
+      });
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({
+          message: "User Not Found For Specified Id",
+        });
+      }
+    })
+    .catch(() => {
+      res.status(400).json({
+        message: "User Id Must Be Number",
+      });
+    });
+};
+//get user list
 const findUser = async (req, res) => {
   try {
     const { userName, gitHandle, userId } = req.query;
     if (userName) {
-      let userList = await Users.findAll({
-        where: {
-          name: {
-            [Sequelize.Op.iLike]: "%" + userName + "%",
-          },
-        },
-      });
+      let userList = await findUserByUserName(userName);
       res.status(200).json(userList);
     } else if (gitHandle) {
-      let userList = await Users.findAll({
-        where: {
-          [Sequelize.Op.or]: [
-            {
-              gitlab_handle: {
-                [Sequelize.Op.iLike]: "%" + gitHandle + "%",
-              },
-            },
-            {
-              github_handle: {
-                [Sequelize.Op.iLike]: "%" + gitHandle + "%",
-              },
-            },
-            {
-              bitbucket_handle: {
-                [Sequelize.Op.iLike]: "%" + gitHandle + "%",
-              },
-            },
-          ],
-        },
-      });
+      const userList = await findUserByGitHandle(gitHandle);
       res.status(200).json(userList);
     } else if (userId) {
-      await yup
-        .object()
-        .shape({
-          userId: yup.number().required({ userId: "required" }),
-        })
-        .validate(
-          {
-            userId: userId,
-          },
-          { abortEarly: false }
-        )
-        .then(async () => {
-          let user = await Users.findOne({
-            where: {
-              id: userId,
-            },
-          });
-          if (user) {
-            res.status(200).json(user);
-          } else {
-            res.status(404).json({
-              message: "user not found for given id",
-            });
-          }
-        })
-        .catch(() => {
-          res.status(400).json({
-            message: "user Id must be number",
-          });
-        });
+      await findUserByUserId(res, userId);
     } else {
       res.status(400).json({
-        message: "key value required",
+        message: "Params Required",
       });
     }
   } catch {
     res.status(500).json({
-      message: "internal server error",
+      message: "Internal Server Error",
     });
   }
 };
