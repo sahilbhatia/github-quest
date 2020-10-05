5531d46e2c56870263397f8e95d47fe7106ba582
Avinash Mane <avimanepatil@gmail.com>
2020-10-05 08:39:13 +0530

    modify gitlab util function

--- utils/gitlabFunction.js
+++ utils/gitlabFunction.js
@@ -1,6 +1,7 @@
 const request = require("superagent");
 const moment = require("moment");
 const dbConn = require("../models/sequelize");
+const { Sentry } = require("./sentry");
 dbConn.sequelize;
 const db = require("../models/sequelize");
 const Users = db.users;
@@ -9,154 +10,167 @@ const Users_repositories = db.users_repositories;
 
 //function for insert new repository
 const insertNewRepo = async (insertRepos, repo) => {
-  insertRepos = await Repositories.create({
-    source_type: "gitlab",
-    source_repo_id: repo.id,
-    name: repo.name,
-    url: repo.web_url,
-    description: repo.description,
-    is_disabled: !repo.packages_enabled,
-    is_archived: repo.archived,
-    is_private: repo.visibility == "private" ? true : false,
-    is_forked: repo.forked_from_project ? true : false,
-    created_at: repo.created_at,
-    updated_at: repo.last_activity_at,
-    review: "pending",
-  });
-  return insertRepos;
+  try {
+    insertRepos = await Repositories.create({
+      source_type: "gitlab",
+      source_repo_id: repo.id,
+      name: repo.name,
+      url: repo.web_url,
+      description: repo.description,
+      is_disabled: !repo.packages_enabled,
+      is_archived: repo.archived,
+      is_private: repo.visibility == "private" ? true : false,
+      is_forked: repo.forked_from_project ? true : false,
+      created_at: repo.created_at,
+      updated_at: repo.last_activity_at,
+      review: "pending",
+    });
+    return insertRepos;
+  } catch (err) {
+    Sentry.captureException(err);
+    return null;
+  }
 };
 
 //function for insert forked repo
 const insertForkedRepoFunction = async (forkRepo, repo, insertRepos) => {
-  const insertForkedRepo = await Repositories.create({
-    source_type: "gitlab",
-    source_repo_id: forkRepo.id,
-    name: forkRepo.name,
-    url: forkRepo.web_url,
-    description: forkRepo.description,
-    is_disabled: !forkRepo.packages_enabled,
-    is_archived: forkRepo.archived,
-    is_private: forkRepo.visibility == "private" ? true : false,
-    is_forked: true,
-    created_at: forkRepo.created_at,
-    updated_at: forkRepo.last_activity_at,
-    parent_repo_id: insertRepos
-      ? insertRepos.dataValues.id
-      : null,
-    is_suspicious:
-      (repo.visibility == "private" &&
-        forkRepo.visibility != "private") ||
-        (insertRepos
-          ? insertRepos.dataValues.is_suspicious
-          : false)
-        ? true
-        : false,
-    review:
-      (repo.visibility == "private" &&
-        forkRepo.visibility != "private") ||
-        (insertRepos
-          ? insertRepos.dataValues.is_suspicious
-          : false)
-        ? "suspicious auto"
-        : "no action",
-    reviewed_at:
-      (repo.visibility == "private" &&
-        forkRepo.visibility != "private") ||
-        (insertRepos
-          ? insertRepos.dataValues.is_suspicious
-          : false)
-        ? moment.utc().format()
-        : null,
-  });
-  return insertForkedRepo;
+  try {
+    const insertForkedRepo = await Repositories.create({
+      source_type: "gitlab",
+      source_repo_id: forkRepo.id,
+      name: forkRepo.name,
+      url: forkRepo.web_url,
+      description: forkRepo.description,
+      is_disabled: !forkRepo.packages_enabled,
+      is_archived: forkRepo.archived,
+      is_private: forkRepo.visibility == "private" ? true : false,
+      is_forked: true,
+      created_at: forkRepo.created_at,
+      updated_at: forkRepo.last_activity_at,
+      parent_repo_id: insertRepos ? insertRepos.dataValues.id : null,
+      is_suspicious:
+        (repo.visibility == "private" && forkRepo.visibility != "private") ||
+        (insertRepos ? insertRepos.dataValues.is_suspicious : false)
+          ? true
+          : false,
+      review:
+        (repo.visibility == "private" && forkRepo.visibility != "private") ||
+        (insertRepos ? insertRepos.dataValues.is_suspicious : false)
+          ? "suspicious auto"
+          : "no action",
+      reviewed_at:
+        (repo.visibility == "private" && forkRepo.visibility != "private") ||
+        (insertRepos ? insertRepos.dataValues.is_suspicious : false)
+          ? moment.utc().format()
+          : null,
+    });
+    return insertForkedRepo;
+  } catch (err) {
+    Sentry.captureException(err);
+    return null;
+  }
 };
 
 //function for link repository with user
 const linkUserRepository = async (user, repo) => {
-  await Users_repositories.create({
-    user_id: user.id,
-    repository_id: repo.id,
-  });
-  return null;
+  try {
+    await Users_repositories.create({
+      user_id: user.id,
+      repository_id: repo.id,
+    });
+    return null;
+  } catch (err) {
+    Sentry.captureException(err);
+    return null;
+  }
 };
 
 //function for find repo
 const findRepoFunction = async (id) => {
-  const repo = await Repositories.findOne({
-    where: {
-      source_repo_id: id.toString(),
-    },
-  });
-  if (!repo) {
-    return false;
-  } else {
-    return repo;
+  try {
+    const repo = await Repositories.findOne({
+      where: {
+        source_repo_id: id.toString(),
+      },
+    });
+    if (!repo) {
+      return false;
+    } else {
+      return repo;
+    }
+  } catch (err) {
+    Sentry.captureException(err);
+    return null;
   }
 };
 
 //function for update repository
 const updateRepo = async (insertParentRepo, insertRepos, ParentRepo, repo) => {
-  await Repositories.update(
-    {
-      parent_repo_id: insertParentRepo.id,
-      is_suspicious:
-        ParentRepo.visibility == "private" &&
-          repo.visibility != "private"
-          ? true
-          : false,
-      review:
-        ParentRepo.visibility == "private" &&
-          repo.visibility != "private"
-          ? "suspicious auto"
-          : "no action",
-      reviewed_at:
-        ParentRepo.visibility == "private" &&
-          repo.visibility != "private"
-          ? moment.utc().format()
-          : null,
-      manual_review: false,
-    },
-    {
-      where: {
-        id: insertRepos.id,
+  try {
+    await Repositories.update(
+      {
+        parent_repo_id: insertParentRepo.id,
+        is_suspicious:
+          ParentRepo.visibility == "private" && repo.visibility != "private"
+            ? true
+            : false,
+        review:
+          ParentRepo.visibility == "private" && repo.visibility != "private"
+            ? "suspicious auto"
+            : "no action",
+        reviewed_at:
+          ParentRepo.visibility == "private" && repo.visibility != "private"
+            ? moment.utc().format()
+            : null,
+        manual_review: false,
       },
-    }
-  );
-  return null;
+      {
+        where: {
+          id: insertRepos.id,
+        },
+      }
+    );
+    return null;
+  } catch (err) {
+    Sentry.captureException(err);
+    return null;
+  }
 };
 
 //function for update forked repo
 const updateForkedRepo = async (insertRepos, forkRepo, repo) => {
-  const updateObject = {
-    is_forked: true,
-    parent_repo_id: insertRepos.id,
-    is_suspicious:
-      (repo.visibility == "private" &&
-        forkRepo.visibility != "private") ||
+  try {
+    const updateObject = {
+      is_forked: true,
+      parent_repo_id: insertRepos.id,
+      is_suspicious:
+        (repo.visibility == "private" && forkRepo.visibility != "private") ||
         insertRepos.is_suspicious
-        ? true
-        : false,
-    review:
-      (repo.visibility == "private" &&
-        forkRepo.visibility != "private") ||
+          ? true
+          : false,
+      review:
+        (repo.visibility == "private" && forkRepo.visibility != "private") ||
         insertRepos.is_suspicious
-        ? "suspicious auto"
-        : "no action",
-    reviewed_at:
-      (repo.visibility == "private" &&
-        forkRepo.visibility != "private") ||
+          ? "suspicious auto"
+          : "no action",
+      reviewed_at:
+        (repo.visibility == "private" && forkRepo.visibility != "private") ||
         insertRepos.is_suspicious
-        ? moment.utc().format()
-        : null,
-    manual_review: false,
-    updated_at: forkRepo.last_activity_at,
-  };
-  await Repositories.update(updateObject, {
-    where: {
-      source_repo_id: forkRepo.id.toString(),
-    },
-  });
-  return null;
+          ? moment.utc().format()
+          : null,
+      manual_review: false,
+      updated_at: forkRepo.last_activity_at,
+    };
+    await Repositories.update(updateObject, {
+      where: {
+        source_repo_id: forkRepo.id.toString(),
+      },
+    });
+    return null;
+  } catch (err) {
+    Sentry.captureException(err);
+    return null;
+  }
 };
 
 //insert gitlab repositories
@@ -177,7 +191,10 @@ module.exports.insertGitlabRepos = async (databaseUser) => {
           let insertRepos;
           if (repo.visibility != "private") {
             insertRepos = await insertNewRepo(insertRepos, repo);
-            await linkUserRepository(databaseUser.dataValues, insertRepos.dataValues);
+            await linkUserRepository(
+              databaseUser.dataValues,
+              insertRepos.dataValues
+            );
           }
           //get parent repo
           if (repo.forked_from_project) {
@@ -190,13 +207,24 @@ module.exports.insertGitlabRepos = async (databaseUser) => {
             if (!findRepo) {
               let insertParentRepo;
               if (ParentRepo.body.visibility != "private") {
-                insertRepos = await insertNewRepo(insertParentRepo, ParentRepo.body);
+                insertRepos = await insertNewRepo(
+                  insertParentRepo,
+                  ParentRepo.body
+                );
                 if (gitlabUser.body[0].id == ParentRepo.creator_id) {
-                  await linkUserRepository(databaseUser.dataValues, insertParentRepo.dataValues);
+                  await linkUserRepository(
+                    databaseUser.dataValues,
+                    insertParentRepo.dataValues
+                  );
                 }
               }
               if (repo.visibility != "private") {
-                await updateRepo(insertParentRepo.dataValues, insertRepos.dataValues, ParentRepo.body, repo);
+                await updateRepo(
+                  insertParentRepo.dataValues,
+                  insertRepos.dataValues,
+                  ParentRepo.body,
+                  repo
+                );
               }
             } else {
               if (repo.visibility != "private") {
@@ -222,14 +250,25 @@ module.exports.insertGitlabRepos = async (databaseUser) => {
               const findRepo = await findRepoFunction(forkRepo.id);
               if (findRepo) {
                 if (repo.visibility != "private") {
-                  await updateForkedRepo(insertRepos.dataValues, forkRepo, repo);
+                  await updateForkedRepo(
+                    insertRepos.dataValues,
+                    forkRepo,
+                    repo
+                  );
                 }
               } else {
                 if (forkRepo.visibility != "private") {
-                  const insertForkedRepo = await insertForkedRepoFunction(forkRepo, repo, insertRepos);
+                  const insertForkedRepo = await insertForkedRepoFunction(
+                    forkRepo,
+                    repo,
+                    insertRepos
+                  );
 
                   if (gitlabUser.body[0].id == forkRepo.creator_id) {
-                    await linkUserRepository(databaseUser.dataValues, insertForkedRepo.dataValues);
+                    await linkUserRepository(
+                      databaseUser.dataValues,
+                      insertForkedRepo.dataValues
+                    );
                   }
                 }
               }
@@ -271,14 +310,25 @@ module.exports.insertGitlabRepos = async (databaseUser) => {
               let insertParentRepo;
               if (ParentRepo.body.visibility != "private") {
                 //insert repo
-                insertParentRepo = await insertNewRepo(insertParentRepo, ParentRepo.body);
+                insertParentRepo = await insertNewRepo(
+                  insertParentRepo,
+                  ParentRepo.body
+                );
                 if (gitlabUser.body[0].id == ParentRepo.creator_id) {
                   //link to the user
-                  await linkUserRepository(databaseUser.dataValues, insertParentRepo.dataValues);
+                  await linkUserRepository(
+                    databaseUser.dataValues,
+                    insertParentRepo.dataValues
+                  );
                 }
               }
               if (repo.visibility != "private") {
-                await updateRepo(insertParentRepo.dataValues, findRepo.dataValues, ParentRepo.body, repo);
+                await updateRepo(
+                  insertParentRepo.dataValues,
+                  findRepo.dataValues,
+                  ParentRepo.body,
+                  repo
+                );
               }
             } else {
               if (repo.visibility != "private") {
@@ -308,9 +358,16 @@ module.exports.insertGitlabRepos = async (databaseUser) => {
                 }
               } else {
                 if (forkRepo.visibility != "private") {
-                  const insertForkedRepo = await insertForkedRepoFunction(forkRepo, repo, findRepo);
+                  const insertForkedRepo = await insertForkedRepoFunction(
+                    forkRepo,
+                    repo,
+                    findRepo
+                  );
                   if (gitlabUser.body[0].id == forkRepo.creator_id) {
-                    await linkUserRepository(databaseUser.dataValues, insertForkedRepo.dataValues);
+                    await linkUserRepository(
+                      databaseUser.dataValues,
+                      insertForkedRepo.dataValues
+                    );
                   }
                 }
               }
@@ -353,7 +410,8 @@ module.exports.insertGitlabRepos = async (databaseUser) => {
       );
     }
     return;
-  } catch {
+  } catch (err) {
+    Sentry.captureException(err);
     return;
   }
 };
