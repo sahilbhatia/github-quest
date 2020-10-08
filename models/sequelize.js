1b05236a875c25cecc0f95715db58fdc660fa994
Avinash Mane <avimanepatil@gmail.com>
2020-10-08 11:30:28 +0530

    modify sequelize.js file

--- models/sequelize.js
+++ models/sequelize.js
@@ -1,5 +1,8 @@
 "use strict";
 const Sequelize = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const modelsDirectory = path.resolve(process.cwd(), "models");
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
@@ -15,22 +18,43 @@ if (config.use_env_variable) {
     config
   );
 }
+
+fs.readdirSync(modelsDirectory)
+  .filter((file) => {
+    //console.log(__dirname,file)
+    return (
+      file.indexOf(".") !== 0 &&
+      file !== "sequelize.js" &&
+      file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    const model = sequelize["import"](path.join(modelsDirectory, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.roles = require("./roles.model")(sequelize, Sequelize);
-db.users = require("./users.model")(sequelize, Sequelize);
-db.repositories = require("./repositories.model")(sequelize, Sequelize);
-db.users_repositories = require("./users_repositories.model")(
-  sequelize,
-  Sequelize
-);
-db.projects = require("./projects.model")(sequelize, Sequelize);
-db.projects_repositories = require("./projects_repositories.model")(
-  sequelize,
-  Sequelize
-);
-db.users_projects = require("./users_projects.model")(sequelize, Sequelize);
-db.commits = require("./commits.model")(sequelize, Sequelize);
+// db.roles = require("./roles.model")(sequelize, Sequelize);
+// db.users = require("./users.model")(sequelize, Sequelize);
+// db.repositories = require("./repositories.model")(sequelize, Sequelize);
+// db.users_repositories = require("./users_repositories.model")(
+//   sequelize,
+//   Sequelize
+// );
+// db.projects = require("./projects.model")(sequelize, Sequelize);
+// db.projects_repositories = require("./projects_repositories.model")(
+//   sequelize,
+//   Sequelize
+// );
+// db.users_projects = require("./users_projects.model")(sequelize, Sequelize);
+// db.commits = require("./commits.model")(sequelize, Sequelize);
 
 module.exports = db;
