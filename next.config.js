1c8132320e46cdd6f0fc10eb4fc2af8858212b41
Avinash Mane <avimanepatil@gmail.com>
2020-10-09 17:11:13 +0530

    update next.config file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withSourceMaps = require('@zeit/next-source-maps')();
 
 module.exports = withSourceMaps({
   webpack: (config, options) => {
-   
+
     if (!options.isServer) {
       config.resolve.alias['@sentry/node'] = '@sentry/browser';
     }
