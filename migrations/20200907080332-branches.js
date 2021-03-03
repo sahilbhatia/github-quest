c636aaf60be17b56d815ff38e27d3c52ed11436e
Jitu007-Bunde <jitendrasbunde@gmail.com>
2021-03-03 13:21:03 +0530

    add the model for branches and also  add the migration file for branches

---
+++ migrations/20200907080332-branches.js
@@ -0,0 +1,47 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      "branches",
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: Sequelize.TEXT,
+          allowNull: false,
+        },
+        head_commit_id: {
+          type: Sequelize.STRING(50),
+          allowNull: false,
+        },
+        repository_id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "repositories",
+            key: "id",
+          },
+        },
+        created_at: {
+          type: "TIMESTAMP",
+        },
+        updated_at: {
+          type: "TIMESTAMP",
+        },
+      },
+      {
+        timestamp: false,
+        createdAt: false,
+        updatedAt: false,
+      }
+    );
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable("branches");
+  },
+};
